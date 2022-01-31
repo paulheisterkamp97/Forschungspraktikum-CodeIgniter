a157e61700e6f6b827473587f8869a3ed7f10b99
Paul Heisterkamp <s4paheis@uni-trier.de>
2022-01-31 15:19:36 +0100

    added viewport

--- app/Views/pages/upload.php
+++ app/Views/pages/upload.php
@@ -12,12 +12,12 @@
         <?php endif; ?>
         <form method="post" action="<?=base_url('store-image')?>" enctype="multipart/form-data">
             <div class="form-group mb-4">
-                <label class="form-label fs-5" for="image">Select Picture to upload</label>
-                <input type="file" class="form-control form-control-lg" id="image" accept="image/jpeg,image/png" name="image" size="33" />
+                <label class="form-label" for="image">Select Picture to upload</label>
+                <input type="file" class="form-control" id="image" accept="image/jpeg,image/png" name="image" size="33" />
             </div>
             <div class="form-group mb-4">
-                <label class="form-label fs-5" for="filename">Specify project name</label>
-                <input type="text" id="filename"  class="form-control form-control-lg" name="filename"/>
+                <label class="form-label" for="filename">Specify project name</label>
+                <input type="text" id="filename"  class="form-control" name="filename"/>
             </div>
             <div class="form-group mb-4">
                 <button type="submit" class="btn-lg btn-primary" >Upload Image</button>
